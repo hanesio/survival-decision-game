16a1f459d55eb9003ad097a1a5be0b49c7ba926e
Hannes Richter <60668114+hanesio@users.noreply.github.com>
2025-04-15 16:04:45 +0200

    SessionCard Anzeige Spieler Gruppen

--- sdg-front/src/components/SessionCard.vue
+++ sdg-front/src/components/SessionCard.vue
@@ -14,6 +14,14 @@ const props = defineProps({
         type: Boolean,
         default: false,
     },
+    singlesCount: {
+        type: Number,
+        default: 0,
+    },
+    groupCount: {
+        type: Number,
+        default: 0,
+    },
 });
 
 const newDate = new Date(props.date);
@@ -30,7 +38,7 @@ const nameLength = computed(() => {
 </script>
 <template>
     <div
-        class="lg:w-74 hover:scale-102 relative flex h-40 w-full cursor-pointer flex-col items-center justify-between rounded-2xl border-2 border-cyan-200 bg-cyan-900 p-4 pb-6 transition hover:bg-cyan-700"
+        class="lg:w-74 hover:scale-102 relative flex h-40 w-full cursor-pointer flex-col items-center justify-between rounded-2xl border-2 border-cyan-200 bg-cyan-900 transition hover:bg-cyan-700"
     >
         <div
             class="absolute -top-0 flex h-6 w-14 items-center justify-center rounded-b-xl border-2 border-t-0 border-green-600 bg-green-300 p-2 text-center text-sm font-semibold"
@@ -40,24 +48,34 @@ const nameLength = computed(() => {
         </div>
         <div
             v-if="name.length < 18"
-            class="flex h-full w-full items-center justify-center p-2 text-center text-3xl text-gray-100"
+            class="flex h-full w-full items-center justify-center px-2 pt-3 text-center text-3xl text-gray-100"
         >
             {{ name }}
         </div>
         <div
             v-if="name.length >= 18 && name.length < 38"
-            class="flex h-full w-full items-center justify-center hyphens-auto p-2 text-center text-2xl text-gray-100"
+            class="flex h-full w-full items-center justify-center hyphens-auto px-2 pt-3 text-center text-2xl text-gray-100"
         >
             {{ name }}
         </div>
         <div
             v-if="name.length >= 38"
-            class="flex h-full items-center justify-center hyphens-auto p-2 text-center text-xl text-gray-100"
+            class="flex h-full items-center justify-center hyphens-auto px-2 pt-3 text-center text-xl text-gray-100"
         >
             {{ name }}
         </div>
+        <div class="flex w-full justify-center gap-4 bg-cyan-800 p-1">
+            <div class="flex gap-2">
+                <p class="text-cyan-500">{{ singlesCount }}</p>
+                <p class="text-white">Spieler</p>
+            </div>
+            <div class="flex gap-2">
+                <p class="text-cyan-500">{{ groupCount }}</p>
+                <p class="text-white">Gruppen</p>
+            </div>
+        </div>
 
-        <div class="text-cyan-500">{{ formattedDate }}</div>
+        <div class="py-2 text-sm text-cyan-200">{{ formattedDate }}</div>
     </div>
 </template>
 
--- sdg-front/src/views/admin/AdminHome.vue
+++ sdg-front/src/views/admin/AdminHome.vue
@@ -3,6 +3,7 @@ import SessionCard from '@/components/SessionCard.vue';
 import { useRouter } from 'vue-router';
 import { AxiosHelper } from '@/AxiosHelper';
 import { ref } from 'vue';
+import { getgroups } from 'process';
 
 const axiosHelper = new AxiosHelper();
 
@@ -10,9 +11,13 @@ const router = useRouter();
 
 let sessions = ref([]);
 const active = ref();
+const singles = ref();
+const groups = ref();
 
 getActive(); // Reihenfolge ist wichtig
 getSessions();
+getSingles();
+getGroups();
 
 async function getSessions() {
     const sessionData = await axiosHelper.get('sessions/find');
@@ -23,6 +28,15 @@ async function getActive() {
     active.value = activeData.data;
 }
 
+async function getSingles() {
+    const data = await axiosHelper.get('singles/find');
+    singles.value = data.data;
+}
+
+async function getGroups() {
+    const data = await axiosHelper.get('groups/find');
+    groups.value = data.data;
+}
 function openSession(id: number) {
     router.push(`/admin/sessions/${id}`);
 }
@@ -39,12 +53,14 @@ function openSession(id: number) {
             <p class="text-xl">NEUE SESSION</p>
         </div>
         <SessionCard
-            v-if="active"
+            v-if="active && singles && groups"
             @click="openSession(session._id)"
             v-for="session in sessions.slice().reverse()"
             :name="session.sessionname"
             :date="session.date"
             :is-active="active.sessionId === session._id"
+            :group-count="groups.filter((group) => group.sessionId === session._id).length"
+            :singles-count="singles.filter((single) => single.sessionId === session._id).length"
         />
     </div>
 </template>
