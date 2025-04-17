2ea65559fba754659f3cdec14655fb91357ee327
Hannes Richter <60668114+hanesio@users.noreply.github.com>
2025-04-17 15:16:10 +0200

    fix:handle SPA

--- server/index.js
+++ server/index.js
@@ -200,7 +200,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 // Handle SPA
-// app.get('*', (req, res) => res.sendFile(path.join(__dirname + 'public/index.html')));
+app.get('*', (req, res) => res.sendFile(path.join(path.resolve() + '/public/index.html')));
 
 const PORT = process.env.PORT || 5000;
 
