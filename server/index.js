a0e9017d264758d855fddf7fc830d55b3f3c1faf
Hannes Richter <60668114+hanesio@users.noreply.github.com>
2025-05-16 10:27:25 +0200

    fix: spa 3

--- server/index.js
+++ server/index.js
@@ -263,7 +263,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 // Handle SPA
-app.get('/{*splat}', (req, res) => res.sendFile(path.resolve(process.cwd(), '/index.html')));
+app.get('/*splat', (req, res) => res.sendFile(path.resolve() + '/public/index.html'));
 
 const PORT = process.env.PORT || 5000;
 
